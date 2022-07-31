62b9f985a008ca94ab73fef47488c25ccdaf3153
Mai_Max <62517731+Mai-Tubo@users.noreply.github.com>
2022-07-31 17:53:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,10 @@ server = Flask(__name__)
 
 @bot.message_handler(content_types=['start'])
 def start(message):
-    bot.send_message(message.from_user.id, "Привіт, надсилай свої ідеї чи контент")       
+    if message.text == '/start':
+        bot.send_message(message.from_user.id, "Привіт, надсилай свої ідеї чи контент")  
+    else:
+        bot.send_message(message.from_user.id, "Помилка! Буль-ласка, повторіть ще раз!")          
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
@@ -29,13 +32,13 @@ def getMessage():
     json_string = request.get_data().decode('utf-8')
     update = telebot.types.Update.de_json(json_string)
     bot.process_new_updates([update])
-    return "!", 200
+    return "Hello, world!", 200
 
 @server.route("/")
 def webhook():
     bot.remove_webhook()
     bot.set_webhook(url=APP_NAME + TOKEN)
-    return "!", 200    
+    return "Hello, world!", 200    
 
 if __name__ == "__main__":
     server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
