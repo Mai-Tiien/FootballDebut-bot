186323a1f4ec34dbe54e6620d539c380dc3a9078
Mai_Max <62517731+Mai-Tubo@users.noreply.github.com>
2022-08-02 11:14:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,45 @@
 import telebot
 import os
 from flask import Flask, request
-from news import *
+from bs4 import BeautifulSoup
+import requests
+from googletrans import Translator
 
 TOKEN = '5499977311:AAFd2fY862MCTE8c4JNvcDybVCWXZQxS-Sg'
 APP_NAME='https://footballduet-bot.herokuapp.com/'
 bot = telebot.TeleBot(TOKEN)
 
 server = Flask(__name__)       
+
+headers = {'User-agent': 'Mozilla/5.0'}
+
+request = requests.get('https://www.skysports.com/football/news', headers=headers)
+html = request.content
+
+soup = BeautifulSoup(html, 'html.parser')
+
+def football(keyword):
+    news_list = []
+
+    for h in soup.findAll('a', class_='news-list__headline-link'):
+        news_title = h.contents[0].lower()
+
+        if news_title not in news_list:
+            if 'skysports' not in news_title:
+                news_list.append(news_title)
+
+    with open('out.txt', 'w', encoding='utf-8') as file:
+        for i, title in enumerate(news_list):
+            text = title.strip()
+            translator = Translator()
+            trans = translator.translate(text, dest='ru')
+            res = i+1, trans.text
+            
+            tee = str(res)+'\n'
+            file.write(tee.replace("(", "").replace(")","").replace("'","").replace(",","."))
+    with open('out.txt', encoding='utf-8') as f:
+        contents = f.read()        
+    return contents
 
 @bot.message_handler(func=lambda message: True, content_types=['text', 'photo'])
 def echo_message(message):
